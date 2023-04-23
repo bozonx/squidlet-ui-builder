450e2c88b68da9de899f2f875eeb8d9f75b52b86
Ivan <ipkozyrin@gmail.com>
2023-04-23 19:35:58 +0300

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
@@ -12,10 +10,7 @@
 <section>
 	<h1>
 		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
+
 		</span>
 
 		to your new<br />SvelteKit app
---
+++ src/routes/builder/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <title>Ui builder</title>
+  <meta name="description" content="The UI for build apps for squidlet" />
+</svelte:head>
+
+<script>
+  import RootElemetsMenu from '$lib/components/RootElemetsMenu.svelte';
+</script>
+
+<div>
+  <RootElemetsMenu />
+</div>
