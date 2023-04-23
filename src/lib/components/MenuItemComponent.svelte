e359a6a495b7b2bfaaf742e7bedfd33bf1f31eef
Ivan <ipkozyrin@gmail.com>
2023-04-23 20:01:49 +0300

    add

---
+++ src/lib/components/MenuItemComponent.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let item = {}
+</script>
+
+<div>
+  <input value={item.name} />
+  <textarea>{item.template}</textarea>
+</div>
--- src/lib/components/RootElemetsMenu.svelte
+++ src/lib/components/RootElemetsMenu.svelte
@@ -1,6 +1,18 @@
 <script>
+  import MenuItemComponent from "./MenuItemComponent.svelte";
+
+  const testComponents = [
+    {
+      name: 'My component',
+      template: '<div>some content</div>',
+    }
+  ]
+
 </script>
 
 <div>
   gggg
+  {#each testComponents as item}
+    <MenuItemComponent item={item} />
+  {/each}
 </div>
