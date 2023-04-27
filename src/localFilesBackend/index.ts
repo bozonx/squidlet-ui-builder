b84b6c48d5e1b7e58f4d4f07b6327b1957a43a19
Ivan <ipkozyrin@gmail.com>
2023-04-28 02:28:19 +0300

    add

---
+++ src/localFilesBackend/index.ts
@@ -0,0 +1,71 @@
+import Koa from 'koa'
+import path from 'node:path';
+import * as fs from 'node:fs/promises';
+
+
+const app = new Koa();
+
+
+const relSrcDirPath = process.argv[2]
+
+if (!relSrcDirPath) {
+  throw new Error(`Specify source dir path please`)
+}
+
+const absDirPath = path.resolve(process.cwd(), relSrcDirPath)
+
+
+app.use(async (ctx, next) => {
+
+  // console.log(
+  //   222,
+  //
+  //   //ctx.req.url,
+  //   //ctx.request.href,
+  //   ctx.request.path,
+  //   //ctx.request.querystring,
+  //   //ctx.request.search,
+  //   //ctx.request.URL,
+  //   //ctx.request.type,
+  //   //ctx.request.charset,
+  //   ctx.request.query,
+  //   //ctx.is('application/json'),
+  // )
+
+  if (ctx.req.method === 'GET' && ctx.request.path === '/load' && ctx.request.query.file) {
+    ctx.response.type = 'application/json'
+
+    // TODO: лучше ограничить доступ наверх
+    const filePath = path.join(absDirPath, ctx.request.query.file as string)
+    let fileContentStr: string
+
+    try {
+      fileContentStr = await fs.readFile(filePath, 'utf8')
+    }
+    catch (e) {
+      ctx.body = JSON.stringify({
+        error: {
+          message: `Can't load a file: ${e}`
+        }
+      })
+      ctx.response.status = 404
+
+      return
+    }
+
+    ctx.body = JSON.stringify({
+      result: fileContentStr
+    })
+    ctx.response.status = 200
+  }
+  else {
+    ctx.body = JSON.stringify({
+      error: {
+        message: 'Wrong url'
+      }
+    })
+    ctx.response.status = 404
+  }
+});
+
+app.listen(3000)
