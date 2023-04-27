f5c4071f92f9640a8aeab211cbeff25c9922ff0d
Ivan <ipkozyrin@gmail.com>
2023-04-27 19:48:25 +0300

    add

--- src/system/ItemResource.ts
+++ src/system/ItemResource.ts
@@ -1,6 +1,11 @@
-import {LocalFiles} from '../dataAdapters/LocalFiles.js';
+import {LocalFiles} from './dataAdapters/LocalFiles.js';
 
 export class ItemResource {
+
+  constructor(adapterName: string, adapterConfig: Record<string, any>) {
+
+  }
+
   run() {
     const aa = new LocalFiles()
   }
