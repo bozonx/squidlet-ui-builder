603aaecfaff88d4c842fa01bd45a6cd0243424df
Ivan <ipkozyrin@gmail.com>
2023-04-30 00:06:38 +0300

    change

--- src/system/DataAdapterBase.ts
+++ src/system/DataAdapterBase.ts
@@ -5,6 +5,7 @@ import {ListStore} from '../types/ListStore.js';
 
 export class DataAdapterBase<Config = Record<string, any>> {
   protected config: Config
+  private stores: Record<string, (ListStore | ItemStore)> = {}
 
 
   constructor(config: Config) {
@@ -15,6 +16,24 @@ export class DataAdapterBase<Config = Record<string, any>> {
   protected makeInstanceId(): string {
     // TODO: make it
     return '0'
+  }
+
+  /**
+   * Register new store in there isn't any registered.
+   * If it is already registered then just return it
+   * @param storeId - has to be some unique id like file path
+   */
+  protected registerOrGetStore<T = ItemStore | ListStore>(
+    storeId: string,
+    instatiate: () => T
+  ): T {
+    const instanceId = this.makeInstanceId()
+    // TODO: use destroy
+    const destroyFn = async () => this.destroyInstance(instanceId)
+
+    this.registerInstance(instanceId, dataStore)
+
+    return instatiate()
   }
 
   protected registerInstance(instanceId: string, dataStore: ItemStore | ListStore) {
--- src/system/dataAdapters/LocalFiles.ts
+++ src/system/dataAdapters/LocalFiles.ts
@@ -1,5 +1,6 @@
 import yaml from 'yaml'
 import axios, {AxiosProgressEvent, AxiosResponse} from 'axios';
+import {pathJoin} from 'squidlet-lib';
 import {DataAdapterBase} from '../DataAdapterBase.js';
 import {makeItemStore} from '../makeItemStore.js';
 import {ItemStore} from '../../types/ItemStore.js';
@@ -7,7 +8,6 @@ import {ListStore} from '../../types/ListStore.js';
 import {makeListStore} from '../makeListStore.js';
 
 
-// TODO: не используется
 interface LocalFilesConfig {
   basePath: string
 }
@@ -20,44 +20,43 @@ export class LocalFiles extends DataAdapterBase<LocalFilesConfig> {
 
 
   dirContent(params: {path: string}): ListStore {
-    const instanceId = this.makeInstanceId()
-    // TODO: use destroy
-    const destroyFn = async () => this.destroyInstance(instanceId)
-    // TODO: если уже есть этот стор то просто брать его
-    const dataStore: ListStore = makeListStore([])
+    const storeId = this.makeFullFilePath(params.path)
+    const store = this.registerOrGetStore<ListStore>(
+      storeId,
+      () => makeListStore([])
+    )
 
-    this.registerInstance(instanceId, dataStore)
     this.makeRequest<{result: string[]}>(`load-dir?path=${encodeURIComponent(params.path)}`)
       .then((response) => {
         const result = response.data.result
 
-        dataStore.$$setValue(result, false, false, result.length)
+        store.$$setValue(result, false, false, result.length)
       })
       .catch(this.handleRequestError)
 
     // TODO: listen file updates and update value of dataStore
 
-    return dataStore
+    return store
   }
 
   dataFile(params: {path: string}): ItemStore {
-    const instanceId = this.makeInstanceId()
-    // TODO: use destroy
-    const destroyFn = async () => this.destroyInstance(instanceId)
-    const dataStore: ItemStore = makeItemStore(null)
+    const storeId = this.makeFullFilePath(params.path)
+    const store = this.registerOrGetStore<ItemStore>(
+      storeId,
+      () => makeItemStore(null)
+    )
 
-    this.registerInstance(instanceId, dataStore)
     this.makeRequest<{result: string}>(`load-file?path=${encodeURIComponent(params.path)}`)
       .then((response) => {
         const dataObj = yaml.parse(response.data.result)
 
-        dataStore.$$setValue(dataObj)
+        store.$$setValue(dataObj)
       })
       .catch(this.handleRequestError)
 
     // TODO: listen file updates and update value of dataStore
 
-    return dataStore
+    return store
   }
 
   // dirContent(params: {path: string}): ListStore {
@@ -92,6 +91,14 @@ export class LocalFiles extends DataAdapterBase<LocalFilesConfig> {
     console.log(e)
 
     // TODO: handle error
+  }
+
+  private makeFullFilePath(paramPath: string): string {
+    if (this.config?.basePath) {
+      return pathJoin(this.config.basePath, paramPath)
+    }
+
+    return paramPath
   }
 
 }
