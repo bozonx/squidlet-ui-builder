81a5cd35018bf305d43b6f0c8b8190ddb5363ab8
Ivan <ipkozyrin@gmail.com>
2023-05-13 12:57:04 -0300

    add

--- src/AbstractUi/ComponentsPool.ts
+++ src/AbstractUi/ComponentsPool.ts
@@ -3,15 +3,22 @@ import yaml from 'yaml';
 import {STD_COMPONENTS} from './StdComponents.js';
 
 
+// TODO: использовать пользовательские компоненты тоже
+
+
 export class ComponentsPool {
+  // line {stdComponentNameOrPathToUserComponent: ComponentDefinition}
+  private parsedDefinitions: Record<string, ComponentDefinition> = {}
 
 
-  getComponentDefinition(name: keyof typeof STD_COMPONENTS): ComponentDefinition {
-    const definitionStr = STD_COMPONENTS[name]
+  getComponentDefinition(componentName: keyof typeof STD_COMPONENTS): ComponentDefinition {
+    if (this.parsedDefinitions[componentName]) return this.parsedDefinitions[componentName]
 
-    // TODO: использовать пользовательские компоненты тоже
-    // TODO: кэшироввать
+    const definitionStr = STD_COMPONENTS[componentName]
 
-    return yaml.parse(definitionStr)
+    this.parsedDefinitions[componentName] = yaml.parse(definitionStr)
+
+    return this.parsedDefinitions[componentName]
   }
+
 }
