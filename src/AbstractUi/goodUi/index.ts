211b1204add2dc7c8c5bcbf19a8ce4288d321954
Ivan <ipkozyrin@gmail.com>
2023-05-15 11:07:04 -0300

    refactor

--- src/AbstractUi/goodUi/index.ts
+++ src/AbstractUi/goodUi/index.ts
@@ -4,6 +4,11 @@ import {Nav} from './Nav.js';
 import {Text} from './Text.js';
 import {VerticalMenu} from './VerticalMenu.js';
 import {NestedMenu} from './NestedMenu.js';
+import {MainSection} from './MainSection.js';
+import {SideMenu} from './SideMenu.js';
+import {Link} from './Link.js';
+import {Container} from './Container.js';
+import {ExternalLink} from './ExternalLink.js';
 
 
 export const GOOD_UI: Record<string, string> = {
@@ -13,4 +18,9 @@ export const GOOD_UI: Record<string, string> = {
   NestedMenu,
   Text,
   VerticalMenu,
+  ExternalLink,
+  Container,
+  Link,
+  MainSection,
+  SideMenu,
 }
--- src/AbstractUi/stdLib/index.ts
+++ src/AbstractUi/stdLib/index.ts
@@ -1,21 +1,10 @@
 import {Document} from './Document.js';
-import {MainSection} from './MainSection.js';
-import {SideMenu} from './SideMenu.js';
-import {Link} from './Link.js';
 import {ForEach} from './ForEach.js';
 import {RenderComponent} from './RenderComponent.js';
-import {Container} from './Container.js';
-import {ExternalLink} from './ExternalLink.js';
-
 
 
 export const STD_COMPONENTS: Record<string, string> = {
-  Container,
   Document,
-  ExternalLink,
   ForEach,
-  Link,
-  MainSection,
   RenderComponent,
-  SideMenu,
 }
